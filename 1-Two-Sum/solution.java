4463b78fdf08ebe496e322cd21776021fc3103f1
Danielyao0312 <danielyao0312@gmail.com>
2016-03-27 10:14:22 -0400

    [1. Two Sum][Accepted]committed by Daniel

---
+++ 1-Two-Sum/solution.java
@@ -0,0 +1,17 @@
+public class Solution{
+	public int[] twoSum(int[] nums, int target){
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int[] result = new int[2];
+		int len = nums.length;
+		for(int i = 0; i < len; i++) {
+			if(map.containsKey(target - nums[i])){
+				result[0] = map.get(target - nums[i]);
+				result[1] = i;
+				break;
+            }else{
+        	    map.put(nums[i], i);
+            }
+        }
+		return result;
+    }
+}
