6cc59d561b94c5bbf3f854f6ae50b833b00b73de
Daniel <danielyao0312@gmail.com>
2016-04-07 17:16:53 -0400

    changeBack

--- 1-Two-Sum/solution.java
+++ 1-Two-Sum/solution.java
@@ -1,7 +1,4 @@
 public class Solution{
-	//this is for something to commit
-	//add for roll back
-
 	public int[] twoSum(int[] nums, int target){
 		HashMap<Integer, Integer> map = new HashMap<>();
 		int[] result = new int[2];
