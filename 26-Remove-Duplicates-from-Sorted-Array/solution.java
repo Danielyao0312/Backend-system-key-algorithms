72005527c8ef02e5d1f59ef94964cd1189b1519c
Daniel Yao <danielyao0312@gmail.com>
2016-04-08 16:47:33 -0400

    [26. Remove Duplicates from Sorted Array][Accepted]committed by Daniel

--- 26-Remove-Duplicates-from-Sorted-Array/solution.java
+++ 26-Remove-Duplicates-from-Sorted-Array/solution.java
@@ -1,18 +1,20 @@
 public class Solution {
+    //题目关键字：数组，已排好序的，只能in place替换，不能用额外数组，并且要求结果返回unique的长度。
+    // 题解方法是双指针，一个指针只记录unique的并且帮助记录长度，一个指针往前找。
+    // 从 nums[1]开始往后扫，只有遇到与前一个值不相等时，替换，并且维护的指针 p++
     
-    // use two pointers, i and j, i to point to the result array, and j is moving to find the duplicate
-    public int removeDuplicates(int[] nums) {
-        if(nums.length == 0) return 0;
-        
-        // 1,1,1,2,2,3,4
-        
-        int i = 0, j = 1;
-        for(j = 1; j < nums.length; j++){
-            if(nums[j] != nums[i]){
-                i++;
-                nums[i] = nums[j];
+    public int removeDuplicates(int[] nums){
+        if(nums.length == 0 || nums == null) return 0;
+        int p = 1;
+        int duplicate = nums[0];
+        for(int i = 1; i < nums.length; i ++){
+            if(nums[i] != duplicate){
+                nums[p] = nums[i];
+                p++;
+                duplicate = nums[i];
             }
         }
-        return i + 1;
+        return p;
     }
+    
 }
