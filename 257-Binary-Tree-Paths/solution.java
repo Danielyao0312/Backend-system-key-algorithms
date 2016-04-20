1ca24d79f8c46c053a57ada8b954447b0b999c87
Daniel Yao <danielyao0312@gmail.com>
2016-04-20 12:49:02 -0400

    [257. Binary Tree Paths][Accepted]committed by Daniel

--- 257-Binary-Tree-Paths/solution.java
+++ 257-Binary-Tree-Paths/solution.java
@@ -1,57 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> res = new ArrayList<String>();
-        List<StringBuilder> sbres = helper(root);
 
-        for (StringBuilder i : sbres) {
-            res.add(i.toString());
-        }
+
+public class Solution {
+    
+    List<String> res = new ArrayList<String>();
+    
+    public List<String> binaryTreePaths(TreeNode root) {
+        if(root != null) findPaths(root,String.valueOf(root.val));
         return res;
-        
     }
     
-    public List<StringBuilder> helper(TreeNode root) {
-        List<StringBuilder> res = new ArrayList<StringBuilder>();
-        StringBuilder sb = new StringBuilder();
-
-        //base cases:
-        if (root == null) {
-            return res;
-        }
-
-        if(root.left == null && root.right == null) {
-            sb.append(root.val);
-            res.add(sb);
-            return res;
-        }
-
-        //divide and recursively do something to left and right
-        List<StringBuilder> resleft = helper(root.left);
-        List<StringBuilder> resright = helper(root.right);
-        for (StringBuilder i : resleft) {
-            String rootval = Integer.toString(root.val);
-            i.insert(0, "->");
-            i.insert(0, rootval);
-        }
-        for (StringBuilder i : resright) {
-            String rootval = Integer.toString(root.val);
-            i.insert(0, "->");
-            i.insert(0, rootval);
-        }
-
-        //combine left and right
-        res.addAll(resleft);
-        res.addAll(resright);
-
-        return res;
+    private void findPaths(TreeNode n, String path){
+        if(n.left == null && n.right == null) res.add(path);
+        if(n.left != null) findPaths(n.left, path+"->"+n.left.val);
+        if(n.right != null) findPaths(n.right, path+"->"+n.right.val);
     }
 }
