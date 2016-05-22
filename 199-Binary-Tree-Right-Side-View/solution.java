d8dd5e92e4bb75b5aa4093f214e1d572f1bda984
Di Yao <danielyao0312@gmail.com>
2016-05-22 14:07:27 -0400

    [199. Binary Tree Right Side View][Accepted]committed by Daniel

--- 199-Binary-Tree-Right-Side-View/solution.java
+++ 199-Binary-Tree-Right-Side-View/solution.java
@@ -1,12 +1,31 @@
-# Definition for a  binary tree node
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution:
-    # @param root, a tree node
-    # @return a list of integers
-    def rightSideView(self, root):
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        //BFS to solve this problem, at the last node of each level, add the node to the result
+        List<Integer> res = new ArrayList<>();
         
+        if(root == null) return res;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode cur = q.poll();
+                if(i == size - 1)   res.add(cur.val);
+                if(cur.left != null)    q.add(cur.left);
+                if(cur.right != null)   q.add(cur.right);
+            }
+        }
+        return res;
+    }
+}
