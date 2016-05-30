6113a6fa244215219c091f4a825a9c1f83c15beb
Di Yao <danielyao0312@gmail.com>
2016-05-30 12:51:17 -0400

    [109. Convert Sorted List to Binary Search Tree][Accepted]committed by Daniel

--- 109-Convert-Sorted-List-to-Binary-Search-Tree/solution.java
+++ 109-Convert-Sorted-List-to-Binary-Search-Tree/solution.java
@@ -16,31 +16,28 @@
  * }
  */
 public class Solution {
-    // solution 1:  use top down (pre order solution) 
+    // solution 1:  use top down (pre order solution) (fast and slow pointer)
     public TreeNode sortedListToBST(ListNode head) {
+        return sortedListToBST(head, null);
+    }
+    
+    private TreeNode sortedListToBST(ListNode start, ListNode end) {
         //base case:
-        if(head == null)    return null;
-        if(head.next == null)   return new TreeNode(head.val); // 出错点 : 建立个树点， 重要！
+        if(start == end)    return null;
         
-        //use slow and fast pointer to find the root node
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode pre = null; 
-        while(fast != null && fast.next != null) {
-            pre = slow; // pre to find the node just before the root node
+        ListNode fast = start;
+        ListNode slow = start;
+        
+        while(fast != end && fast.next != end) {
             fast = fast.next.next;
             slow = slow.next;
         }
         
-        // 此时 slow 指向的是中间的那个点，以他为核心建立 root, 然后，左边的 linked list 建立左子树，右边的建立右子树
         TreeNode root = new TreeNode(slow.val);
-        ListNode rightList = slow.next;
-        slow.next = null;
-        if (pre != null)   pre.next = null; // !! important
-        
-        root.left = sortedListToBST(head);
-        root.right = sortedListToBST(rightList);
+        root.left = sortedListToBST(start, slow);
+        root.right = sortedListToBST(slow.next, end);
         
         return root;
     }
+    
 }
