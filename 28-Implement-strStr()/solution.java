88b3144a0510358923d1bbfdd41196fbcb6bd966
Di Yao <danielyao0312@gmail.com>
2016-05-31 15:02:59 -0400

    [28. Implement strStr()][Accepted]committed by Daniel

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -1,28 +1,21 @@
-public class Solution {
-    public int strStr(String source, String target) {
-        if(source == null || target == null) return -1;
-        int slen = source.length();
-        int tlen = target.length();
-        if(tlen > slen){
-            return -1;
-        }
-        if(tlen == 0){
-            return 0;
-        }
-        
-        int i = 0, j = 0;
-        for(i = 0; i <= slen - tlen; i++) {
-            if(source.charAt(i) == target.charAt(0)){
-                for(j = 0; j < tlen; j++){
-                    if(source.charAt(i+j) != target.charAt(j)){
-                        break;
-                    }
-                }
-                if(j == tlen){
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
+
+
+
+public class Solution{
+	public int strStr(String str, String target) {
+		int slen = str.length();
+		int tlen = target.length();
+
+		if(tlen > slen)		return -1;
+
+		int j = 0;
+		for(int i = 0; i < slen - tlen + 1; i++) {
+			for(j = 0; j < tlen; j++) {
+				if(target.charAt(j) != str.charAt(i + j))
+					break;
+			}
+			if(j == tlen)	return i;
+		}
+		return -1;
+	}	
 }
