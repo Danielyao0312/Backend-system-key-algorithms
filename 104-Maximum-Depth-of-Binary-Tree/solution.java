7d58d723a650981afb9416655c73612711c7bce8
Di Yao <danielyao0312@gmail.com>
2016-06-02 14:54:57 -0400

    [104. Maximum Depth of Binary Tree][Accepted]committed by Daniel

--- 104-Maximum-Depth-of-Binary-Tree/solution.java
+++ 104-Maximum-Depth-of-Binary-Tree/solution.java
@@ -10,7 +10,6 @@
 public class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
-        if(root.left == null && root.right == null) return 1;
         
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
