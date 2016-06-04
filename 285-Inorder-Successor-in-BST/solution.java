61699931f650650e73a50ac8edb1dc4717e7cc49
Di Yao <danielyao0312@gmail.com>
2016-06-04 18:11:04 -0400

    [285. Inorder Successor in BST][Accepted]committed by Daniel

---
+++ 285-Inorder-Successor-in-BST/solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+public class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if(root == null)    return null;
+        TreeNode lastLeftTurned = null;
+        TreeNode parent = root;
+        
+        while(parent.val != p.val) {
+            if(p.val < parent.val) {
+                lastLeftTurned = parent;
+                parent = parent.left;
+            }else {
+                parent = parent.right;
+            }
+            if(parent == null)  return lastLeftTurned;
+        }
+
+        if(parent.right != null) {
+            return findMin(parent.right);
+        }else{
+            return lastLeftTurned;
+        }
+    }
+
+    private TreeNode findMin(TreeNode root) {
+        if(root == null)    return null;
+        while(root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+}
