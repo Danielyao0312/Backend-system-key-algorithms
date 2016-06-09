605e36045a4393e49c64cf9fded817062d540d67
Di Yao <danielyao0312@gmail.com>
2016-06-08 20:56:49 -0400

    [94. Binary Tree Inorder Traversal][Accepted]committed by Daniel

--- 94-Binary-Tree-Inorder-Traversal/solution.java
+++ 94-Binary-Tree-Inorder-Traversal/solution.java
@@ -1,20 +1,46 @@
-public class Solution {
+//my solution:
+public class Solution{
     public List<Integer> inorderTraversal(TreeNode root) {
-    	List<Integer> res = new ArrayList<>();
-    	Stack<TreeNode>	stack = new Stack<>();
-
-        TreeNode cur = root;
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
         
-        while(cur != null || !stack.isEmpty()) {
-            while(cur != null) {
-                stack.push(cur);
-                cur = cur.left;
-            }
-            cur = stack.pop();
+        if (root == null) return res;
+    
+        pushAllLeft(root, stack);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
             res.add(cur.val);
-            cur = cur.right;
+            pushAllLeft(cur.right, stack);
         }
-
         return res;
     }
+
+    public void pushAllLeft(TreeNode node, Stack stack){
+        while (node != null) {
+            stack.add(node);
+            node = node.left;
+        }
+    }
 }
+
+// other solution:
+// public class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//     	List<Integer> res = new ArrayList<>();
+//     	Stack<TreeNode>	stack = new Stack<>();
+
+//         TreeNode cur = root;
+        
+//         while(cur != null || !stack.isEmpty()) {
+//             while(cur != null) {
+//                 stack.push(cur);
+//                 cur = cur.left;
+//             }
+//             cur = stack.pop();
+//             res.add(cur.val);
+//             cur = cur.right;
+//         }
+
+//         return res;
+//     }
+// }
