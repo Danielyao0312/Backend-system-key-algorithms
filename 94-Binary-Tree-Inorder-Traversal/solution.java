82022b02607c674aed54f75a01a1a9266c4a8d12
Di Yao <danielyao0312@gmail.com>
2016-06-08 20:44:06 -0400

    [94. Binary Tree Inorder Traversal][Accepted]committed by Daniel

--- 94-Binary-Tree-Inorder-Traversal/solution.java
+++ 94-Binary-Tree-Inorder-Traversal/solution.java
@@ -2,9 +2,11 @@ public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
     	List<Integer> res = new ArrayList<>();
     	Stack<TreeNode>	stack = new Stack<>();
+        
+        if(root == null)    return res;
 
         TreeNode cur = root;
-
+        
         while(cur != null || !stack.isEmpty()) {
             while(cur != null) {
                 stack.push(cur);
