8cdd36655d4e008678205c84333f286feeef4aa6
Di Yao <danielyao0312@gmail.com>
2016-06-18 11:57:57 -0400

    [169. Majority Element][Accepted]committed by Daniel

--- 169-Majority-Element/solution.java
+++ 169-Majority-Element/solution.java
@@ -1,24 +1,18 @@
 public class Solution {
-	public int majorityElement(int[] nums) {
-		int len = nums.length;
-		if (len == 1) 	return nums[0];
-
-		Map<Integer, Integer> map = new HashMap<>();
-		for (int i : nums) {
-			if (!map.containsKey(i)) {
-				map.put(i, 1);
-			} else {
-				map.put(i, map.get(i) + 1);
-			}
-		}
-    
-        int res = 0;
-		for (int key : map.keySet()) {
-			if (map.get(key) > len / 2) {
-				res = key;
-			}
-		}
-		return res;
+    public int majorityElement(int[] nums) {
+        int major = nums[0], count = 1;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                major = nums[i];
+                count = 1;
+            } else if (nums[i] == major) {
+                count++;
+            } else { // nums[i] != major
+                count--;
+            }
+        }
+        
+        return major;
     }
-
 }
