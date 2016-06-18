ca87ce69998d1c3b67ab0fcfd7a2093d6719e8ae
Di Yao <danielyao0312@gmail.com>
2016-06-18 12:53:21 -0400

    [229. Majority Element II][Accepted]committed by Daniel

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -1,40 +1,53 @@
 public class Solution {
+
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> res = new ArrayList<Integer>();
-        int m = 0, n = 0, cm = 0, cn = 0;
-        for (int a : nums) {
-            if (a == m) {
-                ++cm;
-            } else if (a == n) {
-                ++cn;
-            } else if (cm == 0) {
-                m = a;
-                cm = 1;
-            } else if (cn == 0) {
-                n = a;
-                cn = 1;
+        List<Integer> list = new ArrayList<>();
+        
+        int len = nums.length;
+        if (len == 0)   return list;
+        
+        int num1 = 0, num2 = 0;
+        int count1 = 0, count2 = 0;
+        
+        for (int i = 0; i < len; i++) {
+            int val = nums[i];
+            
+            if (num1 == val) {
+                count1 ++;
+            } else if (num2 == val) {
+                count2 ++;
+            } else if (count1 == 0) {
+                num1 = val;
+                count1 = 1;
+            } else if (count2 == 0) {
+                num2 = val;
+                count2 = 1;
             } else {
-                --cm;
-                --cn;
+                count1 --;
+                count2 --;
             }
         }
-
-        cm = cn = 0;
-        for (int a : nums){
-            if (a == m) {
-                ++cm;
-            } else if (a == n) {
-                ++cn;
-            }
+        
+        if (isMoreThanOneThird(num1, nums)) {
+            list.add(num1);
         }
-
-        if (cm > nums.length / 3) {
-            res.add(m);
+        if (num2 != num1 && isMoreThanOneThird(num2, nums) ) {
+            list.add(num2);
         }
-        if (cn > nums.length / 3) {
-            res.add(n);
-        }
-
-        return res;
+        return list;
+        
     }
+    
+    private boolean isMoreThanOneThird(int val, int[] nums) {
+        int count = 0;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (val == nums[i]) {
+                count++;
+            }
+        }
+        if(count > len / 3)     return true;
+        return false;
+    }
+    
 }
