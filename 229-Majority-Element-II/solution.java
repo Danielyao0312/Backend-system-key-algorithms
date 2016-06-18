e864d9bac21617a4e87b643eaf4cce1ef40100fa
Di Yao <danielyao0312@gmail.com>
2016-06-18 12:55:21 -0400

    [229. Majority Element II][Accepted]committed by Daniel

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -1,17 +1,14 @@
 public class Solution {
 
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        
-        int len = nums.length;
-        if (len == 0)   return list;
-        
-        int num1 = 0, num2 = 0;
-        int count1 = 0, count2 = 0;
-        
-        for (int i = 0; i < len; i++) {
-            int val = nums[i];
-            
+    List<Integer> list = new ArrayList<>();
+    int n = nums.length;
+    if (n == 0) return list;
+    
+    int num1 = 0, num2 = 0; 
+    int count1 = 0, count2 = 0;
+    for (int i = 0; i < n; i++) {
+        int val = nums[i];
             if (num1 == val) {
                 count1 ++;
             } else if (num2 == val) {
@@ -26,28 +23,20 @@ public class Solution {
                 count1 --;
                 count2 --;
             }
-        }
-        
-        if (isMoreThanOneThird(num1, nums)) {
-            list.add(num1);
-        }
-        if (num2 != num1 && isMoreThanOneThird(num2, nums) ) {
-            list.add(num2);
-        }
-        return list;
-        
     }
     
-    private boolean isMoreThanOneThird(int val, int[] nums) {
-        int count = 0;
-        int len = nums.length;
-        for (int i = 0; i < len; i++) {
-            if (val == nums[i]) {
-                count++;
-            }
-        }
-        if(count > len / 3)     return true;
-        return false;
-    }
+    if (isOneThirdMajority(num1, nums)) list.add(num1);
+    if (num1 != num2 && isOneThirdMajority(num2, nums)) list.add(num2);
+    return list;
+}
+
+private boolean isOneThirdMajority(int val, int[] nums) {
+    int n = nums.length;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+        if (nums[i] == val) count++;
+    if (count > n / 3)  return true;
+    else                return false;
+}
     
 }
