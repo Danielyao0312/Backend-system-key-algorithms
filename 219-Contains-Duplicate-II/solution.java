96fb7048d862d57846d6e3600726cbd6c04f535e
Di Yao <danielyao0312@gmail.com>
2016-06-20 17:20:10 -0400

    [219. Contains Duplicate II][Accepted]committed by Daniel

---
+++ 219-Contains-Duplicate-II/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int j = map.get(nums[i]);
+                if(i - j <= k)
+                    return true;
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}
+
