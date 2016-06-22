898e6d89e142da60c53dc3ea97b1d7f6954e01b1
Di Yao <danielyao0312@gmail.com>
2016-06-22 00:11:57 -0400

    [104. Maximum Depth of Binary Tree][Accepted]committed by Daniel

--- 104-Maximum-Depth-of-Binary-Tree/solution.java
+++ 104-Maximum-Depth-of-Binary-Tree/solution.java
@@ -1,15 +1,13 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    public int maxDepth(TreeNode root) {
-        if(root == null)    return 0;
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-    }
-}
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+	"""docstring for Solution"""
+	def maxDepth(self, root):
+		if root is None :
+			return 0
+		return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
