349ee83eaf1be7048d0bb9d504f981dc2495dc81
Di Yao <danielyao0312@gmail.com>
2016-06-23 19:34:28 -0400

    [33. Search in Rotated Sorted Array][Accepted]committed by Daniel

--- 33-Search-in-Rotated-Sorted-Array/solution.java
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -1,31 +1,28 @@
-public class Solution {
-    public int search(int[] A, int target) {
-        //rotated array can be seperated into two segment
+class Solution(object):
+    def search(self,nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         
-        if(A.length == 0 || A == null) return -1;
+        start = 0
+        end = len(nums) - 1
         
-        int start = 0;
-        int end = A.length - 1;
-        while(start <= end) {
-            int mid = start + (end - start)/2;
-            if(target == A[mid]) return mid;
-
-            if(A[mid] >= A[start]){
-                if(A[start] <= target && target <= A[mid]){
-                    end = mid - 1;
-                }else{
-                    start = mid + 1;
-                }
-            }else{
-                if (A[mid] <= target && target <= A[end]) {
-                    start = mid + 1;
-                }else{
-                    end = mid - 1;
-                }
-
-            }
-        }
-        
-        return -1;
-    }
-}
+        while start<=end:
+            mid = start + (end - start) / 2
+            if nums[mid] == target:
+                return mid
+    
+            if nums[mid] >= nums[start]:
+                if target >= nums[start] and target <= nums[mid] :
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if target >= nums[mid] and target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+    
+        return -1   
