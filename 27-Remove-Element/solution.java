42c0f53bbe0c417c8ddfea2a45d76166925a5878
Di Yao <danielyao0312@gmail.com>
2016-07-05 11:31:25 -0400

    [27. Remove Element][Accepted]committed by Daniel

--- 27-Remove-Element/solution.java
+++ 27-Remove-Element/solution.java
@@ -9,8 +9,7 @@ public class Solution {
         int m = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != val) {
-                arr[m] = arr[i];
-                m++;
+                arr[m++] = arr[i];
             }
         }
         return m;
