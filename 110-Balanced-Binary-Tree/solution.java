ff515749ea4a73324b935dfbfeb5084574e0e2f1
Di Yao <danielyao0312@gmail.com>
2016-07-17 21:02:06 -0400

    [110. Balanced Binary Tree][Accepted]committed by Daniel

--- 110-Balanced-Binary-Tree/solution.java
+++ 110-Balanced-Binary-Tree/solution.java
@@ -13,39 +13,41 @@
 public class Solution {
     
     //version 1:
-    public boolean isBalanced(TreeNode root) {
-        return maxDepth(root) > -1;
-    }
+    // public boolean isBalanced(TreeNode root) {
+    //     return maxDepth(root) > -1;
+    // }
 
+    // private int maxDepth(TreeNode root) {
+    //     if (root == null) {
+    //         return 0;
+    //     }
+
+    //     int left = maxDepth(root.left);
+    //     int right = maxDepth(root.right);
+    //     if (left == -1 || right == -1 || Math.abs(left-right) > 1) {
+    //         return -1;
+    //     }
+    //     return Math.max(left, right) + 1;
+    // }
+
+    
+    // version 2:
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        if( Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1)   {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    
     private int maxDepth(TreeNode root) {
-        if (root == null) {
+        if(root == null) {
             return 0;
         }
-
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        if (left == -1 || right == -1 || Math.abs(left-right) > 1) {
-            return -1;
-        }
-        return Math.max(left, right) + 1;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
-
-    
-    //version 2:
-    // public boolean isBalanced(TreeNode root) {
-    //     if(root == null) {
-    //         return true;
-    //     }
-    //     if( Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1)   {
-    //         return false;
-    //     }
-    //     return isBalanced(root.left) && isBalanced(root.right);
-    // }
-    
-    // private int maxDepth(TreeNode root) {
-    //     if(root == null)    return 0;
-    //     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-    // }
     
     
 }
