5c09cc368115893cc983d27b0db82cc6acf92387
Di Yao <danielyao0312@gmail.com>
2016-07-21 11:02:52 -0400

    [78. Subsets][Accepted]committed by Daniel

--- 78-Subsets/solution.py
+++ 78-Subsets/solution.py
@@ -1,24 +1,36 @@
 class Solution(object):
     def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        lis = []
         res = []
-        nums.sort()
-        if nums is None or len(nums) == 0:
-            return res;
-        self.helper(res, lis, nums, 0)
+        self.dfs(sorted(nums), 0, [], res)
         return res
         
-    def helper(self, res, lis, nums, pos):
-        res.append(list(lis))
+    def dfs(self, nums, index, path, res):
+        res.append(path)
+        for i in xrange(index, len(nums)):
+            self.dfs(nums, i+1, path+[nums[i]], res)
         
-        for i in xrange(pos, len(nums)):
-            lis.append(nums[i])
-            self.helper(res, lis, nums, pos + 1)
-            lis.pop()
+    
+    # def subsets(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: List[List[int]]
+    #     """
+    #     lis = []
+    #     res = []
+    #     nums.sort()
+    #     if nums is None or len(nums) == 0:
+    #         return res;
+    #     self.helper(res, lis, nums, 0)
+    #     return res
+        
+    # def helper(self, res, lis, nums, pos):
+    #     aa = list(lis)
+    #     res.append(aa)
+        
+    #     for i in xrange(pos, len(nums)):
+    #         lis.append(nums[i])
+    #         self.helper(res, lis, nums, pos + 1)
+    #         lis.pop()
     
     
             
