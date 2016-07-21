50b39073446689a0f7ca77d9cc87798f9d8c1964
Di Yao <danielyao0312@gmail.com>
2016-07-21 11:19:13 -0400

    [78. Subsets][Accepted]committed by Daniel

--- 78-Subsets/solution.py
+++ 78-Subsets/solution.py
@@ -1,26 +1,10 @@
+
 class Solution(object):
- 
-        
-    """
-    :type nums: List[int]
-    :rtype: List[List[int]]
-    """        
     def subsets(self, nums):
-        lis = []
-        res = []
         nums.sort()
-        if nums is None or len(nums) == 0:
-            return res;
-        self.helper(res, lis, nums, 0)
-        return res
-        
-    def helper(self, res, lis, nums, pos):
-        res.append(list(lis))
-        
-        for i in xrange(pos, len(nums)):
-            lis.append(nums[i])
-            self.helper(res, lis, nums, i + 1)
-            lis.pop()
-    
+        result = [[]]
+        for num in nums:
+            result += [i + [num] for i in result]
+        return result
     
             
