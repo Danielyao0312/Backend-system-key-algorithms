0318da69fdb1755c41f89bb4ebe6113731db6ee5
Di Yao <danielyao0312@gmail.com>
2016-07-25 16:51:07 -0400

    [115. Distinct Subsequences][Accepted]committed by Daniel

--- 115-Distinct-Subsequences/solution.java
+++ 115-Distinct-Subsequences/solution.java
@@ -18,9 +18,11 @@ public class Solution {
         for (int i = 0; i < m + 1; i++) {
             f[i][0] = 1;
         }
-        for (int i = 1; i < n + 1; i++) {
-            f[0][i] = 0;
-        }
+        
+        //不需要赋初值了，因为本身初始化的时候的默认值就为0
+        // for (int i = 1; i < n + 1; i++) {
+        //     f[0][i] = 0; 
+        // } 
         
         for (int i = 1; i < m + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
