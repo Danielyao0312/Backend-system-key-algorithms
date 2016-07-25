0fd3948d88e79e8261a419aa38017c2aa14586c5
Di Yao <danielyao0312@gmail.com>
2016-07-25 16:51:56 -0400

    [115. Distinct Subsequences][Accepted]committed by Daniel

--- 115-Distinct-Subsequences/solution.java
+++ 115-Distinct-Subsequences/solution.java
@@ -4,6 +4,12 @@ public class Solution {
      * @return: Count the number of distinct subsequences
      * 
      * state: f[i][j] 表示 s 的前 i 个配上 t 的前 j 个能有多少个？
+     * function: 
+     * 1. f[i][j] = f[i - 1][j - 1] + f[i - 1][j] if A[i] == B[j]
+     * 2. f[i][j] = f[i - 1][j]  if A[i] != B[j]
+     * 
+     * initial: f(i, 0) = 1, f(0, i) = 0
+     * answer: f[m][n]
      */
     public int numDistinct(String S, String T) {
         if (S.length() < T.length()) {
@@ -18,11 +24,11 @@ public class Solution {
         for (int i = 0; i < m + 1; i++) {
             f[i][0] = 1;
         }
-        
         //不需要赋初值了，因为本身初始化的时候的默认值就为0
-        // for (int i = 1; i < n + 1; i++) {
-        //     f[0][i] = 0; 
-        // } 
+        // for (int i = 1; i < n + 1; i++) { //易错点，从1 开始，因为(0,0)为1
+        //     f[0][i] = 0;
+        // }
+
         
         for (int i = 1; i < m + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
