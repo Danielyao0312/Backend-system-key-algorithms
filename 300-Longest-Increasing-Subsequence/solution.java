52f15c992e1d900de58de3e3398418c242b19507
Di Yao <danielyao0312@gmail.com>
2016-07-27 18:00:35 -0400

    [300. Longest Increasing Subsequence][Accepted]committed by Daniel

--- 300-Longest-Increasing-Subsequence/solution.java
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -3,6 +3,8 @@ public class Solution {
     state: f[i] 表示以 nums[i] 结尾的 LIS 的长度
     function: f[i] = 比 i 小的之前的所有 j, 如果 nums[j] <= nums[i], 那么则f[j] + 1是一个可能候选的 candidate，我们选出这些 candidate 中最大的
     所以:  f[i] = max {f[j] + 1}  (j < i && nums[j] <= nums[i])
+    initial: dp[1...n-1] = 1
+    answer: max{dp[1...n-1]}
     */
     
     public int lengthOfLIS(int[] nums) {
@@ -10,12 +12,10 @@ public class Solution {
             return 0;
         }
         int[] dp = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            dp[i] = 1;
-        }
         int res = 0;
         
         for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1; // 易错点！开始时要把所有的dp 赋值成1， 因为如果举个列子，4,1, 到1的时候，因为下降了，所以它的 dp[1] = 1， 如果不赋值进行比较，则会为0！
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
