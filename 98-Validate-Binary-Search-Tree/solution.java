33a8ceae522bebb715dd9344e52312c50763487d
Di Yao <danielyao0312@gmail.com>
2016-08-03 17:45:06 -0400

    [98. Validate Binary Search Tree][Accepted]committed by Daniel

--- 98-Validate-Binary-Search-Tree/solution.java
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -35,22 +35,22 @@ public class Solution {
 // 大神算法 version 2
 /*根据题目中的定义来实现，其实就是对于每个结点保存左右界，也就是保证结点满足它的左子树的每个结点比当前结点值小，右子树的每个结点比当前结点值大。对于根节点不用定位界，所以是无穷小到无穷大，接下来当我们往左边走时，上界就变成当前结点的值，下界不变，而往右边走时，下界则变成当前结点 值，上界不变。如果在递归中遇到结点值超越了自己的上下界，则返回false，否则返回左右子树的结果。” 
 */
-    // public static boolean isValidBST(TreeNode root) {  
-    //     return rec(root, Long.MIN_VALUE, Long.MAX_VALUE);  
-    // }
+    public static boolean isValidBST(TreeNode root) {  
+        return rec(root, Long.MIN_VALUE, Long.MAX_VALUE);  
+    }
     
-    // // 用最小值和最大值，来限定子树的范围  
-    // public static boolean rec(TreeNode root, long min, long max){  
-    //     if(root == null)    return true;
+    // 用最小值和最大值，来限定子树的范围  
+    public static boolean rec(TreeNode root, long min, long max){  
+        if(root == null)    return true;
           
-    //     // 不在范围内  
-    //     if(root.val <= min || root.val >= max){  
-    //         return false;  
-    //     }  
+        // 不在范围内  
+        if(root.val <= min || root.val >= max){  
+            return false;  
+        }  
           
-    //     // 检查左右子树的合法性并更新上下限  
-    //     return rec(root.left, min, root.val) && rec(root.right, root.val, max);  
-    // }  
+        // 检查左右子树的合法性并更新上下限  
+        return rec(root.left, min, root.val) && rec(root.right, root.val, max);  
+    }  
 
 
 //version 3:
@@ -63,22 +63,26 @@ public class Solution {
      * Check right subtree
      */
      
-    long pred = Long.MIN_VALUE;
+    // long pred = Long.MIN_VALUE;
     
-    public boolean isValidBST(TreeNode root) { 
-        if (root == null)   return true;
+    // public boolean isValidBST(TreeNode root) { 
+    //     if (root == null)   
+    //         return true;
         
-        //left
-        if (!isValidBST(root.left))     return false;
+    //     //left
+    //     if (!isValidBST(root.left))     
+    //         return false;
         
-        //visit
-        if (root.val <= pred)   return false;
-        pred = root.val;
+    //     //visit
+    //     if (root.val <= pred)   
+    //         return false;
+    //     pred = root.val;
         
-        //right
-        if (!isValidBST(root.right)) return false;
-        return true; //otherwise
-    }
+    //     //right
+    //     if (!isValidBST(root.right)) 
+    //         return false;
+    //     return true; //otherwise
+    // }
 	
 
 }
