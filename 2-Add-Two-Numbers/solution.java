92851a7a7ee07a0cb3360affa31d48669fea9041
Di Yao <danielyao0312@gmail.com>
2016-08-03 16:39:16 -0400

    [2. Add Two Numbers][Accepted]committed by Daniel

--- 2-Add-Two-Numbers/solution.java
+++ 2-Add-Two-Numbers/solution.java
@@ -1,3 +1,4 @@
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -6,39 +7,57 @@
  *     ListNode(int x) { val = x; }
  * }
  */
- /*
- 这道题就是给的加数是倒着给的，你返回的结果也是倒着写的，所以进位也反着进就好。
- 维护一个carry，加数大于9时候carry为1，也要注意两个数相加如果大于8的话，要写取模后的值。
- */
- public class Solution{
+
+/*
+3 pointers: 
+p1 => l1
+p2 => l2
+p3 => new list
+
+add every element in p1, p2=> get digit 
+put (digit + carry) as the new node of p3
+
+
+*/
+
+public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(l1==null)
-            return l2;
-        if(l2==null)
-            return l1;
-        
-        int carry = 0;
-        ListNode dummy = new ListNode(-1);
-        ListNode l3 = dummy;
-        
-        while(l1!=null || l2!=null){
-            if(l1!=null){
-                carry += l1.val;
-                l1 = l1.next;
-            }
-            if(l2!=null){
-                carry += l2.val;
-                l2 = l2.next;
-            }
-            
-            l3.next = new ListNode(carry % 10);
-            carry = carry/10;
-            l3 = l3.next;
+        if (l1 == null) {
+        	return l2;
         }
-        // check carry for the last node
-        if(carry == 1) 
-            l3.next=new ListNode(1);
+        if (l2 == null) {
+        	return l1;
+        }
+
+        int carry = 0;
+        ListNode p1, p2, p3, dummy;
+        dummy = new ListNode(0);
+        p3 = dummy;
+        p1 = l1;
+        p2 = l2;
+
+        while (p1 != null || p2 != null) {
+        	if (p1 != null) {
+        		carry += p1.val;
+        		p1 = p1.next;
+        	}
+        	if (p2 != null) {
+        		carry += p2.val;
+        		p2 = p2.next;
+        	}
+
+        	p3.next = new ListNode(carry % 10);
+        	carry = carry / 10;
+
+        	p3 = p3.next;
+        }
+
+        // take care with the last node
+        if (carry == 1) {
+        	p3.next = new ListNode(1);
+        }
+
         return dummy.next;
-    } 
- }
- 
+
+    }
+}
