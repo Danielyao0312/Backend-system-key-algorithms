70032f16974e35a855720d3c0b1ca5b142ffd4b6
Di Yao <danielyao0312@gmail.com>
2016-08-04 18:40:03 -0400

    [49. Group Anagrams][Accepted]committed by Daniel

--- 49-Group-Anagrams/solution.java
+++ 49-Group-Anagrams/solution.java
@@ -14,7 +14,7 @@ public class Solution {
         Map<String, List<String>> map = new HashMap<>();
         
         for (String str : strs) {
-            String sorted = sort(str);
+            String sorted = labelStr(str);
             if (!map.containsKey(sorted)) {
                 map.put(sorted, new ArrayList<String>());
             } 
@@ -26,7 +26,25 @@ public class Solution {
         return res;
     }
     
+    // apple => a1e1l1p2 : O(n)    
+    private String labelStr(String str) {
+        int[] count = new int[256];
+        for (int i = 0; i < str.length(); i++) {
+            // counting sort :
+            // 下标对应字符的 ascii 码，value 对应的是字符个数！
+            count[str.charAt(i)]++; 
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 256; i++) {
+            if (count[i] > 0){
+                sb.append((char)i).append(count[i]);
+            }
+        }
+        return sb.toString();
+    }
     
+    
+    //sort: apple => aelpp:  O(nlogn)
     private String sort(String str) {
         char[] arr = str.toCharArray();
         Arrays.sort(arr);
