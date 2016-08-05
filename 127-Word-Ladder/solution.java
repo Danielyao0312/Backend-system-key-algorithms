9fb7f06ee1cea8eba63de1e2a407e2599272d067
Di Yao <danielyao0312@gmail.com>
2016-08-05 14:49:31 -0400

    [127. Word Ladder][Accepted]committed by Daniel

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -1,17 +1,19 @@
 public class Solution {
     public int ladderLength(String start, String end, Set<String> dict) {
-        Set<String> map = new HashSet<>(); // record visited words in dict so as not to modify dict
-        Queue<String> word = new LinkedList<String>();
-        word.add(start);
+        Set<String> set = new HashSet<>(); // record visited words in dict so as not to modify dict
+        Queue<String> q = new LinkedList<String>();
+        q.add(start);
+        set.add(start);
         
         // BFS
         int depth = 1;
-        while (!word.isEmpty()) {
+        while (!q.isEmpty()) {
             depth ++; // go to next depth
-            int size = word.size();
+            int size = q.size();
             for (int j = 0; j < size; j++) {
-                String currWord = word.poll();
+                String currWord = q.poll();
                 
+                // for all nodes that neighbor to the currentWord
                 // change each letter of currWord
                 for (int i = 0; i < currWord.length(); i++) {
                     char[] currWordArr = currWord.toCharArray();
@@ -21,13 +23,14 @@ public class Solution {
                             continue;
                         }
                         currWordArr[i] = c;
-                        String newWord = new String(currWordArr); // for all nodes that neighbor to the currentWord
+                        String newWord = new String(currWordArr);  // newWord is the neighbor to currentWord
                         if (newWord.equals(end)) {
                             return depth; // reach the end
                         }
-                        if (dict.contains(newWord) && !map.contains(newWord)) {
-                            word.add(newWord);
-                            map.add(newWord);
+                        
+                        if (dict.contains(newWord) && !set.contains(newWord)) {
+                            q.add(newWord);
+                            set.add(newWord);
                         }
                     }
                 }        
