3bd67964212c32a1d95e0ad05d4c88d45f11d7bf
Di Yao <danielyao0312@gmail.com>
2016-08-05 14:39:50 -0400

    [127. Word Ladder][Accepted]committed by Daniel

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -2,33 +2,42 @@ public class Solution {
     public int ladderLength(String start, String end, Set<String> dict) {
         Set<String> map = new HashSet<>(); // record visited words in dict so as not to modify dict
         Queue<String> word = new LinkedList<String>();
-        Queue<Integer> depth = new LinkedList<Integer>();
+        // Queue<Integer> depth = new LinkedList<Integer>();
+        int depth = 1;
         word.add(start);
-        depth.add(1);
+        // depth.add(1);
         
         // BFS
         while (!word.isEmpty()) {
-            String currWord = word.poll();
-            int currDepth = depth.poll();
-            // return depth if a match is found
-            if (currWord.equals(end))    return currDepth;
-            // change each letter of currWord
-            for (int i = 0; i < currWord.length(); i++) {
-                char[] currWordArr = currWord.toCharArray();
-                // try every possible char and check if there's a match in dict
-                for (char c = 'a'; c <= 'z'; c++) {
-                    if (currWordArr[i] == c) {
-                        continue;
+            depth ++;
+            int size = word.size();
+            for (int j = 0; j < size; j++) {
+                String currWord = word.poll();
+                
+                // int currDepth = depth.poll();
+                // return depth if a match is found
+                
+                // change each letter of currWord
+                for (int i = 0; i < currWord.length(); i++) {
+                    char[] currWordArr = currWord.toCharArray();
+                    // try every possible char and check if there's a match in dict
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (currWordArr[i] == c) {
+                            continue;
+                        }
+                        currWordArr[i] = c;
+                        String newWord = new String(currWordArr); // for all nodes that neighbor to the currentWord
+                        if (newWord.equals(end)) {
+                            return depth;
+                        }
+                        if (dict.contains(newWord) && !map.contains(newWord)) {
+                            word.add(newWord);
+                            map.add(newWord);
+                        }
                     }
-                    currWordArr[i] = c;
-                    String newWord = new String(currWordArr);
-                    if (dict.contains(newWord) && !map.contains(newWord)) {
-                        word.add(newWord);
-                        depth.add(currDepth + 1);
-                        map.add(newWord);
-                    }
-                }
+                }        
             }
+            
         }
         
         return 0; // no match is found in dict
