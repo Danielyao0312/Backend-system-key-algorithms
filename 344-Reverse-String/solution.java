a98289a390ce6285d0c4199a56b672ee2060ef60
Di Yao <danielyao0312@gmail.com>
2016-08-08 16:39:36 -0400

    [344. Reverse String][Accepted]committed by Daniel

--- 344-Reverse-String/solution.java
+++ 344-Reverse-String/solution.java
@@ -1,5 +1,27 @@
 public class Solution {
-    public String reverseString(String s) {
+    // public String reverseString(String s) {
+    //     if(s == null || s.length() == 0) return s;
         
+    //     StringBuilder sb = new StringBuilder();
+    //     for(int i = s.length() - 1; i >= 0 ; i--){
+    //         char ss = s.charAt(i);
+    //         sb.append(ss);
+    //     }
+        
+    //     return sb.toString();
+    // }
+    
+    public String reverseString(String str) {
+        char[] arr = str.toCharArray();
+        int i = 0, j = str.length() - 1;
+        
+        while (i < j) {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(arr);
     }
 }
