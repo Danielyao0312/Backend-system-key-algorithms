e088afbb47e950516e67e941637f05a67aeda586
Di Yao <danielyao0312@gmail.com>
2016-08-12 00:57:00 -0400

    [124. Binary Tree Maximum Path Sum][Accepted]committed by Daniel

--- 124-Binary-Tree-Maximum-Path-Sum/solution.java
+++ 124-Binary-Tree-Maximum-Path-Sum/solution.java
@@ -7,43 +7,22 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-
-public class Solution{
-	static class ReturnType{
-		long singlePath;	//with root
-		long maxPath; // without root
-		ReturnType(long singlePath, long maxPath){
-			this.singlePath = singlePath;
-			this.maxPath = maxPath;
-		}
-	}
-
-	public int maxPathSum(TreeNode root) {
-		ReturnType res = findMaxPathWithRootAndWithoutRoot(root);
-		return (int)Math.max(res.singlePath, res.maxPath);
-	}
-
-	private ReturnType findMaxPathWithRootAndWithoutRoot(TreeNode root) {
-		if(root == null)	return new ReturnType(Integer.MIN_VALUE, Integer.MIN_VALUE);
-		
-		ReturnType left = findMaxPathWithRootAndWithoutRoot(root.left);
-		ReturnType right = findMaxPathWithRootAndWithoutRoot(root.right);
-
-		long maxWithRoot = Collections.max(Arrays.asList(
-			left.singlePath + root.val,
-			right.singlePath + root.val,
-			(long)root.val
-		));
-
-		long maxWithoutRoot = Collections.max(Arrays.asList(
-			left.maxPath,
-			right.maxPath,
-			left.singlePath,
-			right.singlePath,
-			left.singlePath + root.val + right.singlePath
-		));
-
-		return new ReturnType(maxWithRoot, maxWithoutRoot);
-	}
+public class Solution {
+    private int maxSum;
+    
+    public int maxPathSum(TreeNode root) {
+        maxSum = Integer.MIN_VALUE;
+        findMax(root);
+        return maxSum;
+    }
+    
+    private int findMax(TreeNode x) {
+        if (x == null)  return 0;
+        int left = findMax(x.left);
+        int right = findMax(x.right);
+        maxSum = Math.max(x.val + left + right, maxSum);
+        int ret = x.val + Math.max(left, right);
+        return (ret > 0) ? ret : 0;
+    }
+    
 }
