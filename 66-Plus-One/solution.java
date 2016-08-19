9885a7633de8ffe9973c17a7115313588dac0505
Di Yao <danielyao0312@gmail.com>
2016-08-19 16:37:14 -0400

    [66. Plus One][Accepted]committed by Daniel

---
+++ 66-Plus-One/solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        
+        if (digits[len - 1] == 9) {
+            // 1. 123999 => 设置一个 carry = 0 
+            int i = 0;
+            int carry = 1;
+            for (i = len - 1; i >= 0; i--) {
+                digits[i] += carry;
+                
+                if (digits[i] == 10) {
+                    carry = 1;
+                    digits[i] = 0;
+                } else {
+                    carry = 0;
+                }
+            }
+            
+            //1. 全部是99999 => 需要新开一个窗口 100000
+            if (digits[0] == 0) {
+                digits = new int[len + 1];
+                digits[0] = 1;
+            }
+            return digits;
+                            
+        } else {
+            digits[len - 1] ++;
+            return digits;
+        }
+    }
+}
