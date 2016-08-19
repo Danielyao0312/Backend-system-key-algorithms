08e82031561f6b94bb9e765230cd72d8c3762377
Di Yao <danielyao0312@gmail.com>
2016-08-19 17:51:08 -0400

    [231. Power of Two][Accepted]committed by Daniel

--- 231-Power-of-Two/solution.java
+++ 231-Power-of-Two/solution.java
@@ -4,25 +4,25 @@ public class Solution {
 Power of two must be 100...000 (one follows by all zeros).
 */    
     
-    // public boolean isPowerOfTwo(int n) {
-    //     if (n <= 0) {
-    //         return false;
-    //     }
-    //     while (n > 0) {
-    //         if (n != 1 && (n & 1) != 0) return false;   // stop if any bit after 1 is not 0
-    //         n >>= 1;
-    //     }
-    //     return true;
-    // }
-    
-// solution 2:
-
     public boolean isPowerOfTwo(int n) {
         if (n <= 0) {
             return false;
         }
-        return (n & n -1) == 0;
+        while (n > 0) {
+            if (n != 1 && (n & 1) != 0) return false;   // stop if any bit after 1 is not 0
+            n >>= 1;
+        }
+        return true;
     }
+    
+// solution 2:
+
+    // public boolean isPowerOfTwo(int n) {
+    //     if (n <= 0) {
+    //         return false;
+    //     }
+    //     return (n & n -1) == 0;
+    // }
     
     
 }
