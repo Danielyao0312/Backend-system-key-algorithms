62aa5b4bac294063153a175396b375a4fd5d30be
Di Yao <danielyao0312@gmail.com>
2016-08-19 00:48:18 -0400

    [122. Best Time to Buy and Sell Stock II][Accepted]committed by Daniel

--- 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.java
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.java
@@ -5,10 +5,13 @@ public class Solution {
         if(prices.length < 2)   return 0;
         
         int total = 0;
-        for (int i = 0; i < prices.length-1; i++) {
-            if (prices[i+1] > prices[i])    total += prices[i+1]-prices[i];
+        for (int i = 1; i < prices.length; i++) {
+            int diff = prices[i] - prices[i - 1];
+            if (diff > 0) {
+                total += diff;
+            }
         }
-    
+        
         return total;
     }
 }
