cc5e95247e81aee1a24e8e1f87aded7fb87294a3
Di Yao <danielyao0312@gmail.com>
2016-08-19 16:45:31 -0400

    [66. Plus One][Accepted]committed by Daniel

--- 66-Plus-One/solution.java
+++ 66-Plus-One/solution.java
@@ -4,9 +4,8 @@ public class Solution {
         
         if (digits[len - 1] == 9) {
             // 1. 123999 => 设置一个 carry = 0 
-            int i = 0;
             int carry = 1;
-            for (i = len - 1; i >= 0; i--) {
+            for (int i = len - 1; i >= 0; i--) {
                 digits[i] += carry;
                 
                 if (digits[i] == 10) {
