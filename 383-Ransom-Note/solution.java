40e03f789dd25104f5552e3601a2b6c44ca9dc41
Di Yao <danielyao0312@gmail.com>
2016-08-19 17:13:32 -0400

    [383. Ransom Note][Accepted]committed by Daniel

---
+++ 383-Ransom-Note/solution.java
@@ -0,0 +1,33 @@
+/*
+1. hash统计magazine各个字母的数量，
+
+*/
+
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+    	if (magazine == null || ransomNote == null) {
+    		return false;
+    	}
+    	if (ransomNote.length() > magazine.length()) {
+    		return false;
+    	}
+
+        int[] count = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+        	int index = magazine.charAt(i) - 'a';
+        	count[index]++;
+        }		
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+        	int index = ransomNote.charAt(i) - 'a';
+        	count[index]--;
+        }		
+
+        for (int i : count) {
+        	if (i < 0) {
+        		return false;
+        	}
+        }
+        return true;
+    }
+}
