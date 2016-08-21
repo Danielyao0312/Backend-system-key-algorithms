ff0a675f071845b1da6c8ee32982c6be099f9e25
Di Yao <danielyao0312@gmail.com>
2016-08-21 19:08:28 -0400

    [1. Two Sum][Accepted]committed by Daniel

--- 1-Two-Sum/solution.java
+++ 1-Two-Sum/solution.java
@@ -1,17 +1,24 @@
-public class Solution{
-	public int[] twoSum(int[] nums, int target){
-		HashMap<Integer, Integer> map = new HashMap<>();
-		int[] result = new int[2];
-		int len = nums.length;
-		for(int i = 0; i < len; i++) {
-			if(map.containsKey(target - nums[i])){
-				result[0] = map.get(target - nums[i]);
-				result[1] = i;
-				break;
-            }else{
-        	    map.put(nums[i], i);
+/*
+1. hashmap : key : nums[i],  value: key
+2. a + b = tar => a = tar - b
+
+*/
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                return res;
+            }
+            else {
+                map.put(nums[i], i);
             }
         }
-		return result;
+        return res;
     }
 }
