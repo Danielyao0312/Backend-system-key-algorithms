d2ba13da8023354f9e4e227ab66501da9513703a
Di Yao <danielyao0312@gmail.com>
2016-08-21 15:56:35 -0400

    [198. House Robber][Accepted]committed by Daniel

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -1,6 +1,6 @@
 /*
 state:
-	 dp[i] = 抢劫前 i 个房间能得到的最大收益
+	 dp[i] = 抢劫前 i 个房间, 能得到的最大收益
 
 function:  1. 如果 nums[i] 被抢，
             dp[i] = dp[i - 2] + nums[i];
@@ -20,18 +20,23 @@ public class Solution {
     	if (nums == null || nums.length == 0) {
     		return 0;
     	}
-
         int n = nums.length;
 
         int[] dp = new int[n + 1];
 
-        dp[0] = 0;
-        dp[1] = nums[0];
-
+        // dp[0] = 0;
+        // dp[1] = nums[0];
+        int last = nums[0];
+        int lastlast = 0;
+        int curt = 0;
+        
         for (int i = 2; i <= n; i++) {
-        	dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]); //出错点，下标的对应，应该为 i - 1
+            curt = Math.max(lastlast + nums[i - 1], last);
+            lastlast = last;
+            last = curt;
+        // 	dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]); //出错点，下标的对应，应该为 i - 1
         }
 
-        return dp[n];
+        return last;
     }
 }
