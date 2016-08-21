8cf69dfcbcf7a5d57b71011a80202f2f7fed07c4
Di Yao <danielyao0312@gmail.com>
2016-08-21 13:49:55 -0400

    [53. Maximum Subarray][Accepted]committed by Daniel

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,5 +1,5 @@
 /*
-using prefixSum calculate the Sn
+using prefixSum calculate the Si - sj 即为 (i, j) 中的和
 找到两个 sn, 使得后面的 si - sj 最大， (i > j) 
 
 
@@ -12,10 +12,10 @@ public class Solution {
         int ans = Integer.MIN_VALUE;
         
         for (int i = 0; i < nums.length; i++) {
-            minSum = Math.min(minSum, sum);
+            minSum = Math.min(minSum, sum); // 找到当前 sum 之前的最小 sum
             
-            sum += nums[i];
-            ans = Math.max(ans, sum - minSum);
+            sum += nums[i];  // 更新得到当前的 sum 
+            ans = Math.max(ans, sum - minSum);  // 用当前 sum - minSum, 得到一个候选的可能的全局的 subarray 和
         }
         return ans;
     }
