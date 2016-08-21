db35b9e6ed0e536fed0d420a39fca01c592ef003
Di Yao <danielyao0312@gmail.com>
2016-08-21 14:52:12 -0400

    [72. Edit Distance][Accepted]committed by Daniel

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -29,7 +29,7 @@ public class Solution {
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1) ) {
-                    dp[i][j] = Math.min( Math.min(dp[i-1][j] + 1, dp[i][j-1] + 1), dp[i-1][j-1]);
+                    dp[i][j] = dp[i - 1][j - 1];
                 } else {
                     dp[i][j] = Math.min( Math.min(dp[i-1][j] + 1, dp[i][j-1] + 1), dp[i-1][j-1] + 1);
                 }
