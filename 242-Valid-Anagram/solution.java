c70b604cc260f25c373b55c78577c624bd4f9841
Di Yao <danielyao0312@gmail.com>
2016-08-21 16:56:22 -0400

    [242. Valid Anagram][Accepted]committed by Daniel

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -1,36 +1,21 @@
+// hashmap 统计两个串的频率即可
+
 public class Solution {
-//     //solution 1: sort
-//     public boolean isAnagram(String s, String t){
-//         if(s.length() != t.length()) return false;
-        
-//         return sort(s).equals(sort(t));
-//     }
-    
-//     private String sort(String s){
-//         char[] ss = s.toCharArray();
-//         Arrays.sort(ss);
-//         return new String(ss);
-//     }
-    
-    
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) {
+        if (s.length() != t.length()) {
             return false;
         }
-
         int[] count = new int[26];
-    
-        for(int i = 0; i < s.length(); i++) {
+        
+        for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
-    
-        for(int x : count) {
-            if(x != 0) return false;
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
         }
-    
         return true;
-
     }
-    
 }
