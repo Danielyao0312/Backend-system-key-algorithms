541ae5b044f006243913c08b9299b4ebdac6cfe0
Di Yao <danielyao0312@gmail.com>
2016-08-22 13:59:20 -0400

    [384. Shuffle an Array][Accepted]committed by Daniel

---
+++ 384-Shuffle-an-Array/solution.java
@@ -0,0 +1,43 @@
+public class Solution {
+    
+    private int[] nums;
+    private int[] original;
+    private Random rand;
+
+    public Solution(int[] nums) {
+        this.original = nums;
+        this.nums = nums.clone();
+        this.rand = new Random();
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        nums = original.clone();
+        return nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        int size = nums.length;
+        
+        for (int i = 0; i < size; i++) {
+            int pos = rand.nextInt(size - i); //不太理解
+            swap(nums, i, i + pos);  // ？？？？为什么是 i + pos?
+        }
+        
+        return nums;
+    }
+    
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    } 
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
