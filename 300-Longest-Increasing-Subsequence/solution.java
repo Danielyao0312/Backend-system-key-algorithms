6a615f6d02a6344bfbb387dd61fef479c92ae678
Daniel Yao <danielyao0312@gmail.com>
2016-08-23 18:08:11 -0400

    [300. Longest Increasing Subsequence][Accepted]committed by Daniel

--- 300-Longest-Increasing-Subsequence/solution.java
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -1,28 +1,40 @@
+/*
+单序列，最长 => DP 问题
+1. state:  f[i] 表示以 i 结尾的，最长 subsequence 是多长？
+2. function: f[i] = Max(f[j]) + 1   (j < i && nums[j] < nums[i])
+3. initial : f[0 .. n] = 1;
+4. ans : Max(f[i])
+
+*/
+
 public class Solution {
-    /*
-    state: f[i] 表示以 nums[i] 结尾的 LIS 的长度
-    function: f[i] = 比 i 小的之前的所有 j, 如果 nums[j] <= nums[i], 那么则f[j] + 1是一个可能候选的 candidate，我们选出这些 candidate 中最大的
-    所以:  f[i] = max {f[j] + 1}  (j < i && nums[j] <= nums[i])
-    initial: dp[1...n-1] = 1
-    answer: max{dp[1...n-1]}
-    */
-    
     public int lengthOfLIS(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        int[] dp = new int[nums.length];
-        int res = 0;
         
-        for (int i = 0; i < nums.length; i++) {
-            dp[i] = 1; // 易错点！开始时要把所有的dp 赋值成1， 因为如果举个列子，4,1, 到1的时候，因为下降了，所以它的 dp[1] = 1， 如果不赋值进行比较，则会为0！
+        int ans = Integer.MIN_VALUE;
+        int localMax = 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        
+        //initial:
+        Arrays.fill(dp, 1);
+        
+        for (int i = 0; i < n; i++) {
+            localMax = 0;
+            
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    localMax = Math.max(localMax, dp[j]);
                 }
             }
-            res = Math.max(res, dp[i]);
+            
+            dp[i] = localMax + 1;
+            ans = Math.max(ans, dp[i]);
         }
-        return res;
+
+        return ans;
+        
     }
 }
