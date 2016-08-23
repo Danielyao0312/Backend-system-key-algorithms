1b60663892b30538275c23ba33a84336f734b91c
Daniel Yao <danielyao0312@gmail.com>
2016-08-23 18:10:56 -0400

    [300. Longest Increasing Subsequence][Accepted]committed by Daniel

--- 300-Longest-Increasing-Subsequence/solution.java
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -21,16 +21,15 @@ public class Solution {
         //initial:
         Arrays.fill(dp, 1);
         
-        for (int i = 0; i < n; i++) {
-            localMax = 0;
-            
-            for (int j = 0; j < i; j++) {
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < i; j++) 
+            {
                 if (nums[j] < nums[i]) {
-                    localMax = Math.max(localMax, dp[j]);
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
             
-            dp[i] = localMax + 1;
             ans = Math.max(ans, dp[i]);
         }
 
