63c76a795f801fbf8832833a821cecea65929491
Daniel Yao <danielyao0312@gmail.com>
2016-08-24 16:58:41 -0400

    [144. Binary Tree Preorder Traversal][Accepted]committed by Daniel

--- 144-Binary-Tree-Preorder-Traversal/solution.java
+++ 144-Binary-Tree-Preorder-Traversal/solution.java
@@ -9,19 +9,20 @@
  */
 public class Solution {
     
-    List<Integer> res = new ArrayList<>();
+    
     
     public List<Integer> preorderTraversal(TreeNode root) {
-        helper(root);
+        List<Integer> res = new ArrayList<>();
+        helper(res, root);
         return res;
     }
     
-    private void helper(TreeNode root) {
+    private void helper(List<Integer> res, TreeNode root) {
         if (root == null) {
             return;
         }
         res.add(root.val);
-        helper(root.left);
-        helper(root.right);
+        helper(res, root.left);
+        helper(res, root.right);
     }
 }
