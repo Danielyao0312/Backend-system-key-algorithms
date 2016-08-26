934fc907816834497fb9f001f823184a09aab7c7
Daniel Yao <danielyao0312@gmail.com>
2016-08-26 10:38:13 -0400

    [215. Kth Largest Element in an Array][Accepted]committed by Daniel

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,4 +1,7 @@
 public class Solution {
+    
+    // 方法一：用 minHeap, 大小为 k, 遍历完成 heap 中就有最大的 k 个数，堆顶即为第 k 大！ 
+    // time: O(nlogk), space: O(k)
     public static int findKthLargest(int[] nums, int k) {
         if (k == 0 || nums == null || nums.length == 0) {
             return 0;
