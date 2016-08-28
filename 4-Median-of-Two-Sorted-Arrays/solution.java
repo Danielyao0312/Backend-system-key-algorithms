69396213bd4136ca3045a25da87654e721b9550a
Daniel Yao <danielyao0312@gmail.com>
2016-08-28 19:25:07 -0400

    [4. Median of Two Sorted Arrays][Accepted]committed by Daniel

--- 4-Median-of-Two-Sorted-Arrays/solution.java
+++ 4-Median-of-Two-Sorted-Arrays/solution.java
@@ -8,32 +8,35 @@ public class Solution {
         }
     }
     
-    private double findKth(int[] nums1, int s1, int[] nums2, int s2, int k) {
-        if (nums1.length - s1 > nums2.length - s2) {
+   private double findKth(int[] nums1, int s1, int[] nums2, int s2, int k) {
+        int m = nums1.length - s1;  // length of nums1
+        int n = nums2.length - s2;  // length of nums2
+        
+        if (m > n) {
             return findKth(nums2, s2, nums1, s1, k);
         }
-
-        if (s1 >= nums1.length) {
+        
+        if (m <= 0) {
             return nums2[s2 + k - 1];
         }
-        if (s2 >= nums2.length) {
+        if (n <= 0) {
             return nums1[s1 + k - 1];
         }
-
         if (k == 1) {
             return Math.min(nums1[s1], nums2[s2]);
         }
-
+        
         int mid = k / 2 - 1;
-
-        int nums1Key = s1 + mid < nums1.length ?  nums1[s1 + mid] : Integer.MAX_VALUE;
-        int nums2Key = nums2[s2 + mid];
-
+        int nums1Key = s1 + mid < nums1.length ? 
+                            nums1[s1 + mid] : Integer.MAX_VALUE;
+        
+        int nums2Key = s2 + mid < nums2.length ? 
+                            nums2[s2 + mid] : Integer.MAX_VALUE;
+        
         if (nums1Key < nums2Key) {
             return findKth(nums1, s1 + k / 2, nums2, s2, k - k / 2);
         } else {
             return findKth(nums1, s1, nums2, s2 + k / 2, k - k / 2);
-        } 
-        
+        }
     }
 }
