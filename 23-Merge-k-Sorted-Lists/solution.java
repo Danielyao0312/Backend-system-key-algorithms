5615d0bc4b77bc69d4d7c61a2f819bc98698aa6c
Daniel Yao <danielyao0312@gmail.com>
2016-08-28 16:26:55 -0400

    [23. Merge k Sorted Lists][Accepted]committed by Daniel

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -7,7 +7,48 @@
  * }
  */
 public class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
+    
+    // solution 1:  using priorityQueue
+    // public ListNode mergeKLists(ListNode[] lists) {
+    //     if (lists.length == 0) {
+    //         return null;
+    //     }
+    //     if (lists.length == 1) {
+    //         return lists[0];
+    //     }
+        
+    //     PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
+    //         @Override
+    //         public int compare(ListNode o1, ListNode o2) {
+    //             return o1.val - o2.val;
+    //         }
+    //     });
+
+    //     ListNode dummy = new ListNode(-1);
+    //     ListNode p = dummy;
+
+    //     // O(n * logn) : k is the maxLen of a sublist of lists, n is lists.length
+    //     for (ListNode i : lists) {
+    //         if (i != null) {
+    //             pq.add(i);
+    //         }
+    //     }
+        
+    //     // O(n * k * logn)
+    //     while (!pq.isEmpty()) {
+    //         ListNode cur = pq.poll();
+    //         p.next = cur;
+    //         p = p.next;
+    //         if (cur.next != null) {
+    //             pq.add(cur.next);
+    //         }
+    //     }
+        
+    //     return dummy.next;
+    // }
+    
+    // solution 2: using mergeSort
+      public ListNode mergeKLists(ListNode[] lists) {
         if (lists.length == 0) {
             return null;
         }
@@ -15,31 +56,44 @@ public class Solution {
             return lists[0];
         }
         
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
-            @Override
-            public int compare(ListNode o1, ListNode o2) {
-                return o1.val - o2.val;
-            }
-        });
+        return mergeLists(lists, 0, lists.length - 1);
+    }
 
+    private ListNode mergeLists(ListNode[] lists, int i, int j) {
+        if (i == j) {
+            return lists[i];
+        }
+
+        int mid = (i + j) / 2;
+        ListNode left = mergeLists(lists, i, mid);
+        ListNode right = mergeLists(lists, mid + 1, j);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
         ListNode p = dummy;
 
-        for (ListNode i : lists) {
-            if (i != null) {
-                pq.add(i);
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+                p = p.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+                p = p.next;
             }
+        }
+        if (l1 != null) {
+            p.next = l1;
+        } else {
+            p.next = l2; 
         }
 
-        while (!pq.isEmpty()) {
-            ListNode cur = pq.poll();
-            p.next = cur;
-            p = p.next;
-            if (cur.next != null) {
-                pq.add(cur.next);
-            }
-        }
-        
         return dummy.next;
     }
+    
+    
 }
