2149a84ff634c9e7bc777c24f6a1d32f8c293380
Daniel Yao <danielyao0312@gmail.com>
2016-08-28 18:16:50 -0400

    [4. Median of Two Sorted Arrays][Accepted]committed by Daniel

--- 4-Median-of-Two-Sorted-Arrays/solution.java
+++ 4-Median-of-Two-Sorted-Arrays/solution.java
@@ -1,39 +1,42 @@
 public class Solution {
-	public double findMedianSortedArrays(int[] A, int[] B) {
-	    int k = A.length + B.length;
-	    
-	    if (k % 2 == 0) {
-		    return	(1.0)*(kth(A, 0, B, 0, k/2) + kth(A, 0, B, 0, k/2 + 1)) / 2;    
-	    } else {
-	        return kth(A, 0, B, 0, k/2 + 1);
-	    }
-	    
-	}
-	
-	// 重点理解！！！这一部分的代码，马帅版本
-    public int kth(int[] nums1, int s1, int[] nums2, int s2, int k) {
-        if (nums1.length - s1 > nums2.length - s2) {
-            return kth(nums2, s2, nums1, s1, k);
-        }
-        
-        if (nums1.length == s1) {
-            return nums2[s2 + k - 1];
-        }
-        
-        if (k == 1) {
-            return Math.min(nums1[s1], nums2[s2]);
-        }
-        
-        int a = Math.min(nums1.length - s1, k / 2);
-        int b = k - a;
-        
-        if (nums1[s1 + a - 1] < nums2[s2 + b - 1]) {
-            return kth(nums1, s1 + a, nums2, s2, k - a);
-        } else if (nums1[s1 + a - 1] > nums2[s2 + b - 1]) {
-            return kth(nums1, s1, nums2, s2 + b, k - b);
+    
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int len = A.length + B.length;
+        if (len % 2 == 0) {
+            return 1.0 * (findKth(A, 0, B, 0, len / 2) + findKth(A, 0, B, 0, len / 2 + 1)) / 2;
         } else {
-            return nums1[s1 + a - 1];
+            return findKth(A, 0, B, 0, len / 2 + 1);
         }
     }
-	
+
+    //find kth of two sorted list
+    private double findKth(int[] A, int aStart, int[] B, int bStart, int k) {
+        if (aStart >= A.length) {
+            return B[bStart + k - 1];
+        }
+        if (bStart >= B.length) {
+            return A[aStart + k - 1];
+        }
+
+        if (k == 1) {
+            return Math.min(A[aStart], B[bStart]);
+        }
+
+        int mid = k / 2 - 1;
+
+        if (A.length <= aStart + mid) {
+            return findKth(A, aStart, B, bStart + k / 2, k - k / 2);
+        }
+        else if (B.length <= bStart + mid) {
+            return findKth(A, aStart + k / 2, B, bStart, k - k / 2);
+        }
+        else if (A[aStart + mid] < B[bStart + mid]) {
+            return findKth(A, aStart + k / 2, B, bStart, k - k / 2);
+        }
+        else {  // A[aStart + mid] > B[bStart + mid])
+            return findKth(A, aStart, B, bStart + k / 2, k - k / 2);
+        }
+
+    }
+
 }
