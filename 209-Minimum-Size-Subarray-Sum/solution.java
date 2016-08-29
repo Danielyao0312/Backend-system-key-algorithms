9e79cbc6ef9336b86821fbcd69c5f15a040e57e8
Daniel Yao <danielyao0312@gmail.com>
2016-08-29 17:44:33 -0400

    [209. Minimum Size Subarray Sum][Accepted]committed by Daniel

--- 209-Minimum-Size-Subarray-Sum/solution.java
+++ 209-Minimum-Size-Subarray-Sum/solution.java
@@ -1,24 +1,37 @@
+/*
+
+minLen = MAX_VALUE
+two pointers:
+1. i, j 
+2. move j, calculate sum until sum >= s
+3. update the minLen
+4. sum -= nums[i],  move i forward
+
+O(n)
+
+corner case: 
+*/
+
 public class Solution {
-    public static int minSubArrayLen(int s, int[] arr) {
+    public int minSubArrayLen(int s, int[] arr) {
         if (arr == null || arr.length == 0) {
             return 0;
         }
-
+        
         int minLen = Integer.MAX_VALUE;
-        int j = 0;
-        int sum = 0;
-
+        int j = 0, sum = 0;
+        
         for (int i = 0; i < arr.length; i++) {
             while (j < arr.length && sum < s) {
                 sum += arr[j];
-                j++;
+                j ++;
             }
             if (sum >= s) {
                 minLen = Math.min(minLen, j - i);
             }
             sum -= arr[i];
         }
-
+        
         return minLen == Integer.MAX_VALUE ? 0 : minLen;
     }
 }
