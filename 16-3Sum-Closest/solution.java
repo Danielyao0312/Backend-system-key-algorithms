148a93de11a3a66ecd52f36e59ec2dd8585dac79
Daniel Yao <danielyao0312@gmail.com>
2016-08-29 14:28:19 -0400

    [16. 3Sum Closest][Accepted]committed by Daniel

--- 16-3Sum-Closest/solution.java
+++ 16-3Sum-Closest/solution.java
@@ -1,41 +1,28 @@
 public class Solution {
-    public int threeSumClosest(int[] nums, int target) {
-        if(nums == null || nums.length < 3) return 0;
-        
-        // a + b + c = sum,  if sum > target, move high to left, if sum < target, move low to right, 
-        
-        //始终记住，要维护和返回的变量，放在外边
-        Arrays.sort(nums);
-        int closet = Integer.MAX_VALUE;
+    public int threeSumClosest(int[] arr, int target) {
         int res = 0;
-        
-        for(int i = 0; i < nums.length - 2; i++) {
-            int low = i + 1;
-            int high = nums.length - 1;
-            
-            while(low < high){
-                int sum = nums[i] + nums[low] + nums[high];
-                int distance = Math.abs(sum - target);
-                
-                // if distance < closet, update the closet, and also update the return value
-                if(distance < closet){
-                    closet = distance;
+        int diff = Integer.MAX_VALUE;
+
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            int j = i + 1;
+            int k = arr.length - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (Math.abs(sum - target) < diff) {
                     res = sum;
+                    diff = Math.abs(sum - target);
                 }
-                
-                if(sum < target){
-                    low++;
-                }else if(sum > target){
-                    high--;
-                }else{ // sum = target
+                if (sum == target) {
                     return sum;
+                } else if (sum > target) {
+                    k --;
+                } else {
+                    j ++;
                 }
             }
-            
-            
         }
         
         return res;
-    
     }
 }
