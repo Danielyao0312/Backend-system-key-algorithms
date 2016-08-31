860813c9d35b6426386afc00a8cb0558ca76843b
Daniel Yao <danielyao0312@gmail.com>
2016-08-31 17:22:34 -0400

    [258. Add Digits][Accepted]committed by Daniel

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -1,25 +1,17 @@
 public class Solution {
-    
-    //version 1: loops
-    // public int addDigits(int num) {
-    //     while(num >= 10){
-    //         num = sumOfDigits(num);
-    //     }
-    //     return num;
-    // }
-    
-    // public int sumOfDigits(int num){
-    //     int temp = 0;
-    //     while(num > 0) {
-    //         temp += num % 10;
-    //         num = num / 10;
-    //     }
-    //     return temp;
-    // }
-    // version2
-     public int addDigits(int num) {
-         return 1 + (num - 1) % 9;
+    public int addDigits(int num) {
+        while (num >= 10) {
+            num = sumOfDigits(num);
+        }
+        return num;
     }
     
-    
+    private int sumOfDigits(int num) {
+        int res = 0;
+        while (num > 0) {
+            res += num % 10;
+            num /= 10;
+        }
+        return res;
+    }
 }
